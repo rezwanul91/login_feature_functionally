21150efd113c3298cf7d969eddb0c87387b0a151
rezwanul91 <mohammadhaquer@gmail.com>
2023-10-06 13:14:41 -0400

    update

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -3,6 +3,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -10,17 +12,18 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import base.Base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks extends Base{
 	@Before
-	public void setup() {
+	public void setup() throws MalformedURLException {
 		ChromeOptions options = new ChromeOptions();
-		WebDriverManager.chromedriver().setup();
-		options.addArguments("false");
-		driver = new ChromeDriver(options); 
+		
+	
+		driver = new RemoteWebDriver(new URL("http://3.145.197.131:4444"),options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2)); // Implicit wait
 		System.out.println("before run");
